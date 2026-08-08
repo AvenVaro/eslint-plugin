12f43475db78d559cfd3ec3df62a2837c91fc5bc
Aven Varo <aven.varo@avenvaro.com>
2026-08-08 15:27:22 +0300

    test: extract tryExpectConfigAsync runner utility to simplify test cases

--- tests/integration/infrastructure/editorconfig-provider.tests.js
+++ tests/integration/infrastructure/editorconfig-provider.tests.js
@@ -70,12 +70,6 @@ async function describeAsync() {
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_js_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_js_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -87,44 +81,32 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_js
     charset: 'unset_charset'
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    'indent_size = 10',
-    `insert_final_newline = ${expectedConfig.insert_final_newline}`,
-    `trim_trailing_whitespace = ${expectedConfig.trim_trailing_whitespace}`,
-    `charset = ${expectedConfig.charset}`,
-    `end_of_line = ${expectedConfig.end_of_line}`,
-    '',
-    '[*.md]',
-    'indent_size = 5',
-    'trim_trailing_whitespace = false',
-    '',
-    '[*.js]',
-    `indent_size = ${expectedConfig.indent_size}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      'indent_size = 10',
+      `insert_final_newline = ${expectedConfig.insert_final_newline}`,
+      `trim_trailing_whitespace = ${expectedConfig.trim_trailing_whitespace}`,
+      `charset = ${expectedConfig.charset}`,
+      `end_of_line = ${expectedConfig.end_of_line}`,
+      '',
+      '[*.md]',
+      'indent_size = 5',
+      'trim_trailing_whitespace = false',
+      '',
+      '[*.js]',
+      `indent_size = ${expectedConfig.indent_size}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_js_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_md_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_md_async',
-    'index.md'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -136,44 +118,33 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_md
     charset: 'unset_charset'
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    'indent_size = 10',
-    `insert_final_newline = ${expectedConfig.insert_final_newline}`,
-    'trim_trailing_whitespace = true',
-    `charset = ${expectedConfig.charset}`,
-    `end_of_line = ${expectedConfig.end_of_line}`,
-    '',
-    '[*.md]',
-    `indent_size = ${expectedConfig.indent_size}`,
-    `trim_trailing_whitespace = ${expectedConfig.trim_trailing_whitespace}`,
-    '',
-    '[*.js]',
-    'indent_size = 3'
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      'indent_size = 10',
+      `insert_final_newline = ${expectedConfig.insert_final_newline}`,
+      'trim_trailing_whitespace = true',
+      `charset = ${expectedConfig.charset}`,
+      `end_of_line = ${expectedConfig.end_of_line}`,
+      '',
+      '[*.md]',
+      `indent_size = ${expectedConfig.indent_size}`,
+      `trim_trailing_whitespace = ${expectedConfig.trim_trailing_whitespace}`,
+      '',
+      '[*.js]',
+      'indent_size = 3'
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_md_async',
+    'index.md'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -182,34 +153,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_ta
     indent_size: 4
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `indent_size = ${expectedConfig.indent_size}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `indent_size = ${expectedConfig.indent_size}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -218,34 +177,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_ta
     indent_size: 'string_value'
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `indent_size = ${expectedConfig.indent_size}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `indent_size = ${expectedConfig.indent_size}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_tabWidth_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_tabWidth_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -254,34 +201,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_ta
     indent_size: 4
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `tab_width = ${expectedConfig.tab_width}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `tab_width = ${expectedConfig.tab_width}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_tabWidth_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_tabWidth_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_tabWidth_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.tab,
@@ -290,34 +225,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_ta
     indent_size: 'string_value'
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `tab_width = ${expectedConfig.tab_width}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `tab_width = ${expectedConfig.tab_width}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_tab_indentSizeIsString_tabWidth_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.space,
@@ -326,34 +249,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_sp
     indent_size: 4
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `indent_size = ${expectedConfig.indent_size}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `indent_size = ${expectedConfig.indent_size}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.space,
@@ -362,34 +273,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_sp
     indent_size: 'string_value'
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `indent_size = ${expectedConfig.indent_size}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `indent_size = ${expectedConfig.indent_size}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_tabWidth_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_tabWidth_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.space,
@@ -397,34 +296,22 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_sp
     end_of_line: editorconfigProvider.propertyValue.lf
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `tab_width = ${expectedConfig.tab_width}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `tab_width = ${expectedConfig.tab_width}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_tabWidth_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_tabWidth_async(testTempRootDir) {
-  const paths = integrationTestsTestHelper.createTempPaths(
-    testTempRootDir,
-    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_tabWidth_async',
-    'index.js'
-  );
-
   /** @type {import('editorconfig').Props} */
   const expectedConfig = {
     indent_style: editorconfigProvider.propertyValue.space,
@@ -432,25 +319,19 @@ async function test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_sp
     end_of_line: editorconfigProvider.propertyValue.lf
   };
 
-  const editorconfig = integrationTestsTestHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${expectedConfig.indent_style}`,
-    `tab_width = ${expectedConfig.tab_width}`,
-    `end_of_line = ${expectedConfig.end_of_line}`
-  ]);
-
-  try {
-    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
-
-    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
-
-    expectConfig(actualConfig, expectedConfig);
-  }
-  finally {
-    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
-  }
+  await tryExpectConfigAsync(
+    expectedConfig,
+    integrationTestsTestHelper.convertCodeArrayToCodeString([
+      'root = true',
+      '',
+      '[*.js]',
+      `indent_style = ${expectedConfig.indent_style}`,
+      `tab_width = ${expectedConfig.tab_width}`,
+      `end_of_line = ${expectedConfig.end_of_line}`
+    ]),
+    testTempRootDir,
+    'test_loadConfig_editorconfigFileIsFoundedAndSuccessfullyParsed_space_indentSizeIsString_tabWidth_async'
+  );
 }
 
 async function test_loadConfig_editorconfigFileIsNotFounded_async() {
@@ -499,3 +380,33 @@ function expectConfig(actualConfig, expectedConfig) {
   vitest.expect(actualConfig.charset).toBe(expectedConfig.charset);
 }
 
+/**
+ * @private
+ * @async
+ *
+ * Asynchronously attempts to create temporary configuration files, retrieve the actual
+ * EditorConfig properties, and assert them against the expected configuration.
+ * Automatically cleans up the created temporary directory in the `finally` block.
+ *
+ * @param {import('editorconfig').Props} expectedConfig - The reference configuration object to compare against.
+ * @param {string} editorconfig - The raw content or configuration string for the .editorconfig file.
+ * @param {string} testTempRootDir - The root directory where the temporary test folders are created.
+ * @param {string} dirName - The specific name of the temporary directory for this test case.
+ * @param {string} [fileName='index.js'] - The optional name of the mock target file.
+ *
+ * @returns {Promise<void>} A promise that resolves when the assertion and cleanup are complete.
+ */
+async function tryExpectConfigAsync(expectedConfig, editorconfig, testTempRootDir, dirName, fileName = 'index.js') {
+  const paths = integrationTestsTestHelper.createTempPaths(testTempRootDir, dirName, fileName);
+
+  try {
+    await integrationTestsTestHelper.createTempFilesAsync(paths, editorconfig);
+
+    const actualConfig = editorconfigProvider.getConfig(true, paths.mockTargetFile);
+
+    expectConfig(actualConfig, expectedConfig);
+  }
+  finally {
+    await integrationTestsTestHelper.removeAsync(paths.testTmpDir);
+  }
+}
