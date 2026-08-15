2ad6cae3bc60d54e162cc9e8f494a2dfda986e05
Aven Varo <aven.varo@avenvaro.com>
2026-08-15 15:41:58 +0300

    chore: exporting types

--- eslint-plugin/src/index.d.ts
+++ eslint-plugin/src/index.d.ts
@@ -1,5 +1,17 @@
 import { ESLint } from 'eslint';
 
+import { default as ePropertyValue, EPropertyValue } from './infrastructure/property-value.enum.js';
+
+import {
+  JsIndentOptions,
+  JsIndentOptionsTuple,
+  JsVariableDeclaratorIndentOptions,
+  JsStaticBlockIndentOptions,
+  JsCallExpressionIndentOptions,
+  JsFunctionDeclarationIndentOptions,
+  JsFunctionExpressionIndentOptions
+} from './rules/js/indent.js';
+
 declare const plugin: ESLint.Plugin & {
   rules: {
     'js/indent': typeof import('./rules/js/indent.js').default;
@@ -7,3 +19,15 @@ declare const plugin: ESLint.Plugin & {
 };
 
 export default plugin;
+
+export { ePropertyValue, EPropertyValue };
+
+export {
+  JsIndentOptions,
+  JsIndentOptionsTuple,
+  JsVariableDeclaratorIndentOptions,
+  JsStaticBlockIndentOptions,
+  JsCallExpressionIndentOptions,
+  JsFunctionDeclarationIndentOptions,
+  JsFunctionExpressionIndentOptions
+};
