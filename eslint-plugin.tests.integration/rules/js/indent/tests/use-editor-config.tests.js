e91a48493c5f98d896313310d185b6ac6cfd80d3
Aven Varo <aven.varo@avenvaro.com>
2026-08-15 18:04:36 +0300

    refactor: Transfer of common logic

--- eslint-plugin.tests.integration/rules/js/indent/tests/use-editor-config.tests.js
+++ eslint-plugin.tests.integration/rules/js/indent/tests/use-editor-config.tests.js
@@ -30,14 +30,11 @@ async function describeAsync() {
   );
 }
 
-async function test_indentRule_useEditorconfig_false_async(testTempRootDir) {
-  const brokenSourceCode = testHelper.convertCodeArrayToCodeString([
-    'const condition = true;',
-    'if (condition) {',
-    'console.log("broken alignment");',
-    '}'
-  ]);
+async function test_indentRule_useEditorconfig_true_async(testTempRootDir) {
+  await test_indentRule_useEditorconfig_indentFromConfig_async(testTempRootDir, true, 'test_indentRule_useEditorconfig_true_async');
+}
 
+async function test_indentRule_useEditorconfig_false_async(testTempRootDir) {
   const expectedFixedSourceCode = testHelper.convertCodeArrayToCodeString([
     'const condition = true;',
     'if (condition) {',
@@ -45,36 +42,11 @@ async function test_indentRule_useEditorconfig_false_async(testTempRootDir) {
     '}'
   ]);
 
-  const editorconfig = testHelper.convertCodeArrayToCodeString([
-    'root = true',
-    '',
-    '[*.js]',
-    `indent_style = ${ePropertyValue.space}`,
-    'indent_size = 5',
-    `end_of_line = ${ePropertyValue.lf}`
-  ]);
-
-  await indentRuleTestHelper.tryExpectAsync(
-    testTempRootDir,
-    'test_indentRule_useEditorconfig_false_async',
-    editorconfig,
-    [
-      10,
-      {
-        useEditorconfig: false
-      }
-    ],
-    brokenSourceCode,
-    expectedFixedSourceCode
-  );
-}
-
-async function test_indentRule_useEditorconfig_true_async(testTempRootDir) {
-  await test_indentRule_useEditorconfig_async(testTempRootDir, true, 'test_indentRule_useEditorconfig_true_async');
+  await test_indentRule_useEditorconfig_async(testTempRootDir, false, 'test_indentRule_useEditorconfig_false_async', expectedFixedSourceCode);
 }
 
 async function test_indentRule_useEditorconfig_undefined_async(testTempRootDir) {
-  await test_indentRule_useEditorconfig_async(testTempRootDir, undefined, 'test_indentRule_useEditorconfig_undefined_async');
+  await test_indentRule_useEditorconfig_indentFromConfig_async(testTempRootDir, undefined, 'test_indentRule_useEditorconfig_undefined_async');
 }
 
 //================================
@@ -93,21 +65,15 @@ async function test_indentRule_useEditorconfig_undefined_async(testTempRootDir) 
  * @param {string} testTempRootDir - The root directory path dedicated to storing ephemeral file assets during test execution loops.
  * @param {boolean | undefined} useEditorconfig - Flag indicating whether the underlying engine rule should actively bind to local EditorConfig schemas.
  * @param {string} dirName - The target unique namespace folder allocated specifically for separating this evaluation run.
+ * @param {string} expectedFixedSourceCode - Expected corrected source code.
  *
  * @returns {Promise<void>} A promise that fully resolves once assertions terminate successfully and cleanup actions conclude.
  */
-async function test_indentRule_useEditorconfig_async(testTempRootDir, useEditorconfig, dirName) {
+async function test_indentRule_useEditorconfig_async(testTempRootDir, useEditorconfig, dirName, expectedFixedSourceCode) {
   const brokenSourceCode = testHelper.convertCodeArrayToCodeString([
     'const condition = true;',
     'if (condition) {',
     'console.log("broken alignment");',
-    '}'
-  ]);
-
-  const expectedFixedSourceCode = testHelper.convertCodeArrayToCodeString([
-    'const condition = true;',
-    'if (condition) {',
-    '     console.log("broken alignment");',
     '}'
   ]);
 
@@ -133,4 +99,25 @@ async function test_indentRule_useEditorconfig_async(testTempRootDir, useEditorc
     brokenSourceCode,
     expectedFixedSourceCode
   );
+}
+
+/**
+ * Asynchronously executes an integration test case evaluating rule indentation sizes
+ * derived directly from ambient EditorConfig definitions under structural boundaries.
+ *
+ * @param {string} testTempRootDir - The root directory path dedicated to storing ephemeral file assets during test execution loops.
+ * @param {boolean | undefined} useEditorconfig - Flag indicating whether the underlying engine rule should actively bind to local EditorConfig schemas.
+ * @param {string} dirName - The target unique namespace folder allocated specifically for separating this evaluation run.
+ *
+ * @returns {Promise<void>} A promise that fully resolves once assertions terminate successfully and cleanup actions conclude.
+ */
+async function test_indentRule_useEditorconfig_indentFromConfig_async(testTempRootDir, useEditorconfig, dirName) {
+  const expectedFixedSourceCode = testHelper.convertCodeArrayToCodeString([
+    'const condition = true;',
+    'if (condition) {',
+    '     console.log("broken alignment");',
+    '}'
+  ]);
+
+  await test_indentRule_useEditorconfig_async(testTempRootDir, useEditorconfig, dirName, expectedFixedSourceCode);
 }
