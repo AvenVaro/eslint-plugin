ded48c7fbe214706815e60ea4d72ab1baee4ff46
Aven Varo <aven.varo@avenvaro.com>
2026-09-16 18:33:08 +0300

    feat: extended DisabledProperty type

--- eslint-plugin/src/infrastructure/rules-build-helper.d.ts
+++ eslint-plugin/src/infrastructure/rules-build-helper.d.ts
@@ -2,9 +2,9 @@ import { JSONSchema4 } from 'json-schema';
 import { EPropertyValue } from './property-value.enum.js';
 
 /**
- * Specifies the value of the disabled property, which can be either 'unset' or 'off'.
+ * Specifies the value of the disabled property, which can be either 'unset', 'off', null, undefined.
  */
-export type DisableProperty = EPropertyValue['off'] | EPropertyValue['unset'] | null;
+export type DisableProperty = EPropertyValue['off'] | EPropertyValue['unset'] | null | undefined;
 
 /**
  * A utility helper interface designed to simplify the construction and type generation
